3867a98fce418d892937d8143ac29a666ae8145b
Ikuo Suyama <13306175+ikuo-suyama@users.noreply.github.com>
2017-11-01 15:41:20 +0900

    add http client lib and json data

---
+++ src/client/childcare.ts
@@ -0,0 +1,21 @@
+import request from 'superagent'
+
+export interface Childcare {
+  id: number;
+  shorten_name: string; 
+  name: string; 
+  adress: string; 
+  public: string; 
+  latlon: string; 
+  capacity: string; 
+  url: string; 
+  acceptance_age: string; 
+  extension: string; 
+  temporary: string; 
+}
+
+export interface Application {
+
+}
+
+export default class 
